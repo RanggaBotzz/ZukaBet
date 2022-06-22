43dc4f93ea39f9a0aeb116dde0c34ec232d43530
RanggaBotzz <107257184+RanggaBotzz@users.noreply.github.com>
2022-06-22 13:52:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('BOT BY\n Helga', {
+CFonts.say('BOT BY\n Rangga', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BY Helga Zexs`, {                                            
+CFonts.say(`BY Rangga Botz`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
